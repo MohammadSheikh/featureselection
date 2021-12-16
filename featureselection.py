ea4af86c931ef3697475aae916f4245a8fbbcd78
Mahamadsaad Sheikh <mshei008@ucr.edu>
2021-12-16 00:51:23 -0800

    update algorithms and comments

--- featureselection.py
+++ featureselection.py
@@ -2,27 +2,26 @@ import math
 import pickle as cPickle
 import time
 
-# Bunch of the code in this main function was taken from the professor's sample slides since this is just a trivial driver main function.
 def driver():
     filename = input("Type in the file name to test: ")
     algorithm_choice = int(input("Welcome to Mahamadsaad's Feature Selection Algorithm. Type '1' to use Forward Selection, or '2' to for Backward Elimination: "))
 
     # Retrieving only the first line of the file
-    with open("C:/Users/saadd/Desktop/CS 170/Project 2/Test_data/" + filename) as f:
+    with open(filename) as f:
         first_row = next(f)
         # Getting the number of features by grabbing the length of the first line by initially splitting the string into a list.
         number_of_features = len(first_row.split()) - 1
 
     # File closed above so let's open it again.
-    file = open("C:/Users/saadd/Desktop/CS 170/Project 2/Test_data/" + filename, 'r')
+    file = open(filename, 'r')
 
-    # Getting the number of instances by doing a summation of all the lines in the file. (credit to stackoverflow for help)
+    # Getting the number of instances by doing a summation of all the lines in the file. 
     number_of_instances = sum(1 for line in file)
 
     # Preventing an out of bounds error when accessing a list out of range
     file.seek(0)
 
-    # Quick traversing by separating each line of the file into the list itself. (credit to stackoverflow for help)
+    # Quick traversing by separating each line of the file into the list itself. 
     instances = [[] for index in range(number_of_instances)]
     for index in range(number_of_instances):
         instances[index] = [float(decim) for decim in file.readline().split()]
@@ -70,7 +69,7 @@ def nearest_neighboring(file, set_of_features, number_of_instances, label_object
 
         local_distance = math.sqrt(local_distance)
 
-        #Comparison of neighboring distance and label object to classify. (copied from prof's slides)   
+        # Comparison of neighboring distance and label object to classify.  
         if local_distance < nearest_neighbor_distance:
             nearest_neighbor_distance = local_distance
             nearest_neighbor_location = i
@@ -84,7 +83,7 @@ def leave_one_out_cross_validation(file, set_of_features, number_of_instances):
         label_object = i
         nearest_neighbor_location = nearest_neighboring(file, set_of_features, number_of_instances, label_object)
 
-        # If the label object to classify is equal to the nearest neighbor "label", then add one to the correctly classified number (copied from prof's slides)
+        # If the label object to classify is equal to the nearest neighbor "label", then add one to the correctly classified number 
         if file[label_object][0] == file[nearest_neighbor_location][0]:
             number_correctly_classfied += 1
 
@@ -103,8 +102,7 @@ def forward_selection(file, number_of_features, number_of_instances):
         updating_accuracy = 0.0
         
         for j in range(1, number_of_features + 1):
-
-            # if isempty(intersect(current_set_of_features,k)) Only consider adding, if not already added. (idea from prof)
+            
             if j not in set_of_features:
 
                 # Much quicker option of copying lists efficiently
@@ -112,14 +110,12 @@ def forward_selection(file, number_of_features, number_of_instances):
                 copy_set_of_features.append(j)
                 accuracy = leave_one_out_cross_validation(file, copy_set_of_features, number_of_instances)
                 print ("\tUsing feature(s) ", copy_set_of_features, " accuracy is ", accuracy, "%")
-
-               # (copied from prof's slides) 
+                
                # Making sure that the iterated accuracy gets updated and printed below as well.    
                 if updating_accuracy < accuracy:
                     updating_accuracy = accuracy
                     add_features = j
 
-        # (copied from prof's slides)  
         set_of_features.append(add_features) 
 
         if add_features >= 0:
@@ -147,20 +143,17 @@ def backward_elimination(file, number_of_features, number_of_instances):
 
         for j in range(1, number_of_features + 1):
 
-            # if isNotempty(intersect(current_set_of_features,k)) Only consider adding, if not already added. (idea from prof)
             if j in set_of_features:
                 copy_set_of_features = cPickle.loads(cPickle.dumps(set_of_features))
                 copy_set_of_features.remove(j)
                 accuracy = leave_one_out_cross_validation(file, copy_set_of_features, number_of_instances)
                 print ("\tUsing feature(s) ", copy_set_of_features, " accuracy is ", accuracy, "%")
 
-                # (copied from prof's slides) 
                 # Making sure that the iterated accuracy gets updated and printed below as well.  
                 if updating_accuracy < accuracy:
                     updating_accuracy = accuracy
                     remove_features = j
                     
-        # (copied from prof's slides)   
         set_of_features.remove(remove_features)
 
         if remove_features > 0:
